73a3fbf7060201baab332502045e50e1ba51d6fb
huying <1660224836@qq.com>
2018-12-03 10:11:21 +0800

    整体框架

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@
  * @Author: huying 
  * @Date: 2018-12-03 09:30:40 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-12-03 09:33:25
+ * @Last Modified time: 2018-12-03 10:09:27
  */
 
 var gulp = require('gulp');
 
 // 编译scss
 var sass = require('gulp-sass');
+
+// 起服务
+var server = require('gulp-webserver');
+var url = require('url')
+var path = require('path');
+var fs = require('fs');
 
 
 gulp.task('Sass', function() {
@@ -21,3 +27,26 @@ gulp.task('Sass', function() {
 gulp.task('devWatch', function() {
     return gulp.watch('./src/scss/*.scss', gulp.parallel('Sass'))
 })
+
+// 开发环境
+gulp.task('dev', gulp.parallel('Sass', 'devWatch'))
+
+// 起服务
+
+gulp.task('Server', function() {
+    gulp.src('src')
+        .pipe(server({
+            port: 8060,
+
+            open: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    return res.end('')
+                }
+                paths = pathname === '/' ? 'index.html' : pathname;
+                console.log(pathname)
+                res.end(fs.readFileSync(path.join(__dirname, 'src', paths)))
+            }
+        }));
+});
